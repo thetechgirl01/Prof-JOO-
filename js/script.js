fa781b2cec3303d321a20d596b8e776a70aee62c
thetechgirl01 <joychisom1580@gmail.com>
2023-12-26 00:41:05 +0100

    test

--- js/script.js
+++ js/script.js
@@ -64,3 +64,11 @@ filter_btns.forEach((filter_btn) => {
 });
 
 
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 0) {
+    navbar.classList.add('scrolled');
+  } else {
+    navbar.classList.remove('scrolled');
+  }
+});
+
